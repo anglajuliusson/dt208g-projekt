4fd065933da4fb0220f0ea8ce62ce37d8dc305cb
Ängla Juliusson <angla02@icloud.com>
2025-09-03 09:55:11 +0200

    Inkludera komponenter i routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { CoursesComponent } from './courses/courses.component';
+import { MyscheduleComponent } from './myschedule/myschedule.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'courses', component: CoursesComponent },
+    { path: 'myschedule', component: MyscheduleComponent },
+    { path: '', redirectTo: '/courses', pathMatch: 'full' },
+    { path: '404', component: NotFoundComponent },
+    { path: '**', component: NotFoundComponent }
+];
