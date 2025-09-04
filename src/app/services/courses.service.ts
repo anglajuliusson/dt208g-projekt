be78c5a2b288a9bedb6ad2cdf44b95b60883a584
Ängla Juliusson <angla02@icloud.com>
2025-09-04 18:36:51 +0200

    Hämta innehåll från json-fil

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Courses } from '../model/courses';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
-  constructor() { }
+// Properties
+private url: string = "public/miun_courses.json";
+
+  constructor(private http: HttpClient) { }
+
+// Methods
+getCourses(): Observable<Courses[]> {
+  return this.http.get<Courses[]>(this.url);
+}
 }
