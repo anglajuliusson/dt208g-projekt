72f0e3631c8f2eb342ecb31c7e24802878c21655
Ängla Juliusson <angla02@icloud.com>
2025-09-09 10:09:05 +0200

    Visa och hantera användarens ramschema

--- src/app/myschedule/myschedule.component.ts
+++ src/app/myschedule/myschedule.component.ts
@@ -1,11 +1,28 @@
+import { CommonModule, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { ScheduleService } from '../services/schedule.service';
+import { Courses } from '../model/courses';
 
 @Component({
   selector: 'app-myschedule',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, NgFor],
   templateUrl: './myschedule.component.html',
   styleUrl: './myschedule.component.css'
 })
 export class MyscheduleComponent {
+  selectedCourses: Courses[] = [];
 
+  constructor(private scheduleService: ScheduleService) {}
+
+  ngOnInit() {
+    // Ladda ramschemat
+    this.selectedCourses = this.scheduleService.getCourses();
+  }
+
+  // Ta bort kurs och uppdatera listan
+  removeCourse(courseCode: string) {
+    this.scheduleService.removeCourse(courseCode);
+    this.selectedCourses = this.scheduleService.getCourses();
+  }
 }
